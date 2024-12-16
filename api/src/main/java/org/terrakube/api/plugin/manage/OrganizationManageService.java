b9ff47f48b8e14b69edf1bc20e7e92a94dad2634
Alfredo España <alfespa17@gmail.com>
2024-12-16 15:16:30 -0600

    feat: Migrate to Spring-boot 3.4.0

--- api/src/main/java/org/terrakube/api/plugin/manage/OrganizationManageService.java
+++ api/src/main/java/org/terrakube/api/plugin/manage/OrganizationManageService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.terrakube.api.plugin.scheduler.OrganizationPostCreation;
 import org.terrakube.api.plugin.scheduler.ScheduleJob;
+import org.terrakube.api.repository.OrganizationRepository;
 import org.terrakube.api.repository.TemplateRepository;
 import org.terrakube.api.rs.Organization;
 import org.terrakube.api.rs.template.Template;
@@ -65,6 +66,7 @@ public class OrganizationManageService {
                         "- type: \"terraformApply\"\n" +
                         "  name: \"Terraform Apply from Terraform CLI\"\n" +
                         "  step: 200\n";
+        private final OrganizationRepository organizationRepository;
 
         TemplateRepository templateRepository;
 
@@ -100,17 +102,17 @@ public class OrganizationManageService {
 
         public void postCreationSetup(Organization organization) {
                 templateRepository.save(generateTemplate("Plan", "Running Terraform plan",
-                                Base64.getEncoder().encodeToString(TEMPLATE_PLAN.getBytes()), organization));
+                                Base64.getEncoder().encodeToString(TEMPLATE_PLAN.getBytes()), organizationRepository.findById(organization.getId()).get()));
                 templateRepository.save(generateTemplate("Plan and apply", "Running Terraform plan and apply",
-                                Base64.getEncoder().encodeToString(TEMPLATE_APPLY.getBytes()), organization));
+                                Base64.getEncoder().encodeToString(TEMPLATE_APPLY.getBytes()), organizationRepository.findById(organization.getId()).get()));
                 templateRepository.save(generateTemplate("Destroy", "Running Terraform destroy",
-                                Base64.getEncoder().encodeToString(TEMPLATE_DESTROY.getBytes()), organization));
+                                Base64.getEncoder().encodeToString(TEMPLATE_DESTROY.getBytes()), organizationRepository.findById(organization.getId()).get()));
                 templateRepository.save(generateTemplate("Terraform-Plan/Apply-Cli",
                                 "Running Terraform apply from Terraform CLI",
-                                Base64.getEncoder().encodeToString(TEMPLATE_APPLY_CLI.getBytes()), organization));
+                                Base64.getEncoder().encodeToString(TEMPLATE_APPLY_CLI.getBytes()), organizationRepository.findById(organization.getId()).get()));
                 templateRepository.save(generateTemplate("Terraform-Plan/Destroy-Cli",
                                 "Running Terraform destroy from Terraform CLI",
-                                Base64.getEncoder().encodeToString(TEMPLATE_DESTROY_CLI.getBytes()), organization));
+                                Base64.getEncoder().encodeToString(TEMPLATE_DESTROY_CLI.getBytes()), organizationRepository.findById(organization.getId()).get()));
         }
 
         private Template generateTemplate(String name, String description, String tcl, Organization organization) {
